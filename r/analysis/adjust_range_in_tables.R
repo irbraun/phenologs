0c01912ef7a3506624e17ac3fd2cf01cd2b49561
irbraun <irbraun@iastate.edu>
2019-05-08 16:49:16 -0500

    update to edit score range in place

--- r/analysis/adjust_range_in_tables.R
+++ r/analysis/adjust_range_in_tables.R
@@ -5,24 +5,51 @@ source("/Users/irbraun/NetBeansProjects/term-mapping/r/analysis/utils.R")
 
 
 
-# Creates a new file with normalized scores in the prob 
+
+
+
+# Creates a new file with adjusted range scores in the prob 
 # dir: a directory
-# filename: a csv file in that directory with a 'prob' column.
+# filename: a csv file in that directory with a 'prob' column or 'score' column.
 adjust_range_in_file <- function(dir,filename){
   df <- read(dir,filename)
-  df$prob <- range01(df$prob)
-  df$prob <- round(df$prob, 3)
-  path <- paste(dir,filename,sep="")
-  path <- substring(path,1,nchar(path)-4)
-  path <- paste(path,".normalized.csv",sep="")
-  write.csv(df, file=path, row.names=F)
-  l <<- c(l,sum(is.na(df$prob)))
+  
+  if("prob" %in% colnames(df))
+  {
+    df$prob <- range01(df$prob)
+    df$prob <- round(df$prob, 3)
+    path <- paste(dir,filename,sep="")
+    # Commented out to alter the file in place.
+    #path <- substring(path,1,nchar(path)-4)
+    #path <- paste(path,".0to1.csv",sep="")
+    write.csv(df, file=path, row.names=F)
+  }
+  else if("score" %in% colnames(df)){
+    df$score <- range01(df$score)
+    df$score <- round(df$score, 3)
+    path <- paste(dir,filename,sep="")
+    # Commmented out to alter the file in place.
+    #path <- substring(path,1,nchar(path)-4)
+    #path <- paste(path,".0to1.csv",sep="")
+    write.csv(df, file=path, row.names=F)
+  }
+  else{
+    cat("not able to adjust range in file")
+    quit()
+  }
 }
 
 
 
-# Read in the set of files passed in as arguments and normalize the probabilities column.
-# The files that are passed in should be the x_classprobs.csv files.
+
+
+
+
+
+
+
+# Read in the set of files passed in as arguments and adjust range in the probabilities column.
+# The files that are passed in should be the [info]_classprobs.csv or [info]_eval.csv files.
 # Arguments
 # 1 (string) directory
 # 2 (string) filename
