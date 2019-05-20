bbbec0b03901597e433ab1cd4a6b48c69f3996b3
irbraun <irbraun@iastate.edu>
2019-05-20 13:32:35 -0500

    adding method name labeller

--- r/analysis/subsets_approach1.R
+++ r/analysis/subsets_approach1.R
@@ -50,6 +50,8 @@ subset2class <- hashmap(mappings_df$subset, mappings_df$class)
 
 
 
+
+
 # Figuring out logical thresholds for what fraction of possible edges are important.
 # What's the fraction of possible edges in the network that connect two genes in the same class?
 class_sizes <- unname(table(subsets_df$class))
@@ -121,19 +123,21 @@ summarize_method_for_figure <- function(pred_col_name, df, output_dir){
   # Specify which predictive method to use.
   df$value_to_use <- df[,pred_col_name]
   # Generate the empty table.
-  table <- get_empty_table(OUT_FILE_COLUMNS)
+  table <- get_empty_table(c(OUT_FILE_COLUMNS,"subset_number"))
   # Iterate through all the subsets.
   for (subset in subset_name_list){
     results <- mean_similarity_within_and_between(df, subsets_df, c(subset))
     diff <- (as.numeric(results[3])-as.numeric(results[4]))/sd(df$value_to_use)
-    table[nrow(table)+1,] <- c(subset2group[[subset]], subset2class[[subset]], subset, 0, results, diff)
+    subset_number <- subset2subset_number[[subset]]
+    table[nrow(table)+1,] <- c(subset2group[[subset]], subset2class[[subset]], subset, 0, results, diff, subset_number)
   }
   # Iterate through all the classes.
   for (class in class_name_list){
     subset_names <- unique(subsets_df[subsets_df$class==class,]$subset)
     results <- mean_similarity_within_and_between(df, subsets_df, subset_names)
     diff <- (as.numeric(results[3])-as.numeric(results[4]))/sd(df$value_to_use)
-    table[nrow(table)+1,] <- c(subset2group[[subset_names[[1]]]], class, "all", 1, results, diff)
+    subset_number <- 0
+    table[nrow(table)+1,] <- c(subset2group[[subset_names[[1]]]], class, "all", 1, results, diff, subset_number)
   }
   # Write a method specific file, add method column and return.
   write.csv(table, file=paste(output_dir,"summary_of_",pred_col_name,".csv",sep=""),row.names=F)
@@ -173,27 +177,44 @@ write.csv(summary, file=paste(OUTPUT_DIR,"summary_of_all_methods.csv",sep=""), r
 
 
 # Same thing but using the version that makes generating the figure easy.
+subset2subset_number <- hashmap(names_df$Subset.Symbol, names_df$Subset.Number)
+
 method_dfs <- mclapply(PRED_COLUMN_NAMES, summarize_method_for_figure, df=phenotype_network, output_dir=OUTPUT_DIR, mc.cores=numCores)
 method_df <- do.call("rbind", method_dfs)
 method_df$diff <- as.numeric(method_df$diff)
+method_df$subset_number <- as.numeric(method_df$subset_number)
 method_df <- method_df[method_df$subset!="all",]
 
+# Specifications for each plot.
 max_y <- 8
 min_y <- -2
 step_y <- 2
-
-y_label <- "Sim(within) - Sim(between)"
+y_label <- "Similarity(within) - Similarity(between)"
 x_label <- "Functional Subsets"
 
+# Convert how methods are specified in the table to names that work in the figure.
+method_names <- list(
+  "predefined"="Cur. EQs",
+  "cur_m1_edge"="cm1", 
+  "cur_m2_edge"="cm2", 
+  "pre_m1_edge"="Pred. EQs", 
+  "pre_m2_edge"="Pred. EQs", 
+  "enwiki_dbow"="Doc2Vec", 
+  "jaccard"="Word Set", 
+  "cosine"="Word Bag"
+)
+method_labeller <- function(variable,value){return(method_names[value])}
+
+# Make the figure.
 library(grid)
-ggplot(data=method_df, aes(x=subset, y=diff)) + geom_bar(stat="identity") +
+ggplot(data=method_df, aes(x=reorder(subset,subset_number), y=diff)) + geom_bar(stat="identity") +
   theme_bw() +
   scale_y_continuous(breaks=seq(min_y,max_y,step_y), expand=c(0,0), limits=c(min_y,max_y)) +
   theme(plot.title = element_text(lineheight=1.0, face="bold", hjust=0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) +
   ylab(y_label) +
   xlab(x_label) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5)) +
-  facet_grid(method  ~ .) + 
+  facet_grid(method  ~ ., labeller=method_labeller) + 
   theme(panel.spacing = unit(0.8, "lines"))
 
 
@@ -203,100 +224,6 @@ ggplot(data=method_df, aes(x=subset, y=diff)) + geom_bar(stat="identity") +
 
 
 
-
-
-
-
-
-
-
-
-
-# prepare_one_plot <- function(method_df, x_label, y_label){
-#   max_y <- 5
-#   min_y <- -1
-#   step_y <- 1
-#   diff_col_index <- grepl('diff', colnames(method_df))
-#   method_df$diff <- as.numeric(method_df[,diff_col_index])
-#   method_df <- method_df[method_df$subset!="all",]
-#   
-#   plt <- ggplot(data=method_df, aes(x=subset, y=diff)) + geom_bar(stat="identity") +
-#     theme_bw() +
-#     scale_y_continuous(breaks=seq(min_y,max_y,step_y), expand=c(0,0), limits=c(min_y,max_y)) +
-# 
-#     theme(plot.title = element_text(lineheight=1.0, face="bold", hjust=0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) +
-#     ylab(y_label)
-#   
-#   # Only want to include the x-axis label on some of the plots.
-#   if(x_label==""){
-#     plt <- plt + theme(axis.title.x=element_blank()) 
-#     plt <- plt + theme(axis.text.x=element_blank())
-#   }else{
-#     plt <- plt + xlab(x_label)
-#     plt <- plt + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
-#   }
-#   return(plt)
-# }
-# 
-# 
-# p1 <- prepare_one_plot(method_dfs[[1]], "soetuh", "predefined")
-# p2 <- prepare_one_plot(method_dfs[[2]], "tasdkj", "cur_m1")
-# p3 <- prepare_one_plot(method_dfs[[3]], "", "cur_m2")
-# p4 <- prepare_one_plot(method_dfs[[4]], "", "pre_m1")
-# p5 <- prepare_one_plot(method_dfs[[5]], "", "pre_m2")
-# p6 <- prepare_one_plot(method_dfs[[6]], "", "doc2vec")
-# p7 <- prepare_one_plot(method_dfs[[7]], "", "word set")
-# p8 <- prepare_one_plot(method_dfs[[8]], "categories", "word bag")
-# 
-# 
-# 
-# 
-# 
-# p + facet_grid(rows = vars(drv))
-# 
-# 
-# 
-# library(gridExtra)
-# grid.arrange(p1, p2, p3, p4, p5, p6, p7, p8, nrow = 8)
-# 
-# 
-# 
-# library(grid)
-# grid.newpage()
-# grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), size = "last"))
-# 
-# 
-# 
-# 
-# 
-# a <- method_dfs[[5]]
-# a <- a[a$subset!="all",]
-# a$diff.enwiki_dbow <- as.numeric(a$diff.enwiki_dbow)
-# 
-# p1 <- ggplot(data=a, aes(x=subset, y=diff.enwiki_dbow)) + geom_bar(stat="identity") +
-#   theme_bw() +
-#   scale_y_continuous(breaks=seq(-1,5,1.0), expand=c(0,0), limits=c(-1,5)) +
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5)) +
-#   theme(plot.title = element_text(lineheight=1.0, face="bold", hjust=0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) +
-#   theme(axis.title.x=element_blank()) + 
-#   ylab("doc2vec")
-# 
-# 
-# a <- method_dfs[[3]]
-# a <- a[a$subset!="all",]
-# a$diff.pre_m1_edge <- as.numeric(a$diff.pre_m1_edge)
-# 
-# p2 <- ggplot(data=a, aes(x=subset, y=diff.pre_m1_edge)) + geom_bar(stat="identity") +
-#   theme_bw() +
-#   scale_y_continuous(breaks=seq(-1,5,1.0), expand=c(0,0), limits=c(-1,5)) +
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5)) +
-#   theme(plot.title = element_text(lineheight=1.0, face="bold", hjust=0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), axis.line = element_line(colour = "black")) +
-#   ylab("Predicted EQs") +
-#   xlab("Functional Subsets")
-# 
-# 
-# library(gridExtra)
-# grid.arrange(p1, p2, nrow = 2)
 
 
 
