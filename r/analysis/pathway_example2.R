9f774d668d2a9de2b6224944320754d4708f4dba
irbraun <irbraun@iastate.edu>
2019-07-01 17:24:45 -0500

    updating list of genes for pathway analysis

--- r/analysis/pathway_example2.R
+++ r/analysis/pathway_example2.R
@@ -43,7 +43,7 @@ read <- function(dir,filename){
 
 
 # IDs for the phenotypes and corresponding genes in the pathway.
-query_ids_list <- list(
+query_list <- list(
   c("GRMZM2G422750", 1262),                                   # c2
   c("GRMZM2G026930", 1582,1583,1585,1979,2466,2709,171,213),  # a1
   c("GRMZM2G345717", 174,214),                                # a2
@@ -56,17 +56,45 @@ query_ids_list <- list(
   c("GRMZM2G172795", 173,1335,1584)                           # b1
 )
 
+# Expanded set.
+query_list <- list(
+  c("AT5G07990" ,2792),				#  pr1 
+  c("AT1G10370" ,1848),				#  bz2 
+  c("GRMZM2G172795" ,1584,1335,173),				#  transcription factors 
+  c("GRMZM2G084799" ,2467),				#  transcription factors 
+  c("AT4G39640" ,449),				#  bz2 
+  c("GRMZM5G822829" ,1728,2599,215,220,175,1263),				#  transcription factors 
+  c("AT5G13930" ,2793),				#  c2 
+  c("GRMZM2G026930" ,1585,2466,2709,213,1979,171,1582,1583),				#  a1 *
+  c("AT3G51240" ,2788),				#  fht1 
+  c("AT5G17220" ,1730),				#  bz2 
+  c("GRMZM2G422750" ,1262),				#  c2 *
+  c("GRMZM2G165390" ,228,2326),				#  bz1 *
+  c("AT5G24530" ,1912),				#  fht1 
+  c("GRMZM2G016241" ,229),				#  bz2 *
+  c("AT5G44070" ,1450),				#  bz2 
+  c("GRMZM2G701063" ,1336,169),				#  transcription factors 
+  c("GRMZM5G881887" ,1890,1891),				#  a1 
+  c("AT5G42800" ,2795),				#  a1 
+  c("GRMZM2G086773" ,1889),				#  a1 
+  c("AT3G55120" ,2789),				#  chi1 
+  c("GRMZM2G005066" ,170,1261),				#  transcription factors 
+  c("GRMZM2G345717" ,214,174)				#  a2 *
+)
 
 
 
 
-get_k_best_for_one_method <- function(column_to_use, df, query_id){
+get_k_best_for_one_method <- function(column_to_use, df, query_ids){
   df$value_used <- df[,column_to_use]
-  relevant_ids <- query_id
+  relevant_ids <- query_ids
   relevant_slice <- df[df$phenotype_1 %in% relevant_ids | df$phenotype_2 %in% relevant_ids,]
-  k = nrow(relevant_slice)
-  top_k_rows <- relevant_slice <- relevant_slice[order(-relevant_slice$value_used),][1:k,]
-  top_k_rows$top_k_id <- ifelse(top_k_rows$phenotype_1==query_id, top_k_rows$phenotype_2, top_k_rows$phenotype_1)
+  top_k_rows <- relevant_slice[order(-relevant_slice$value_used),]
+  # We have the ordered edges where either p1 or p2 is a query.
+  # Now we need to throw out edges where they're both the query.
+  top_k_rows <- top_k_rows[!( (top_k_rows$phenotype_1 %in% query_ids) & (top_k_rows$phenotype_2 %in% query_ids) ),]
+  # now take just the id values of the found phenotypes.
+  top_k_rows$top_k_id <- ifelse(top_k_rows$phenotype_1 %in% query_ids, top_k_rows$phenotype_2, top_k_rows$phenotype_1)
   return(top_k_rows$top_k_id)
 }
 
@@ -75,12 +103,12 @@ get_k_best_for_one_method <- function(column_to_use, df, query_id){
 
 # Read in the phenotype network files output from the pipeline using the phenotype descriptions.
 phenotype_network <- read(NETWORKS_DIR,PHENOTYPE_TEXT_EDGES_FILE)
-for(query_id in query_ids_list){
+for(query in query_list){
   # Unpack the query ID information.
-  gene_name <- query_id[1]
-  query_id_values <- as.numeric(query_id[2:length(query_id)])
+  gene_name <- query[1]
+  query_id_values <- as.numeric(query[2:length(query)])
   # Generate the ranked list of similar genes.
-  table <- mclapply(PRED_COLUMN_NAMES, get_k_best_for_one_method, df=phenotype_network, query_id=query_id_values, mc.cores=numCores)
+  table <- mclapply(PRED_COLUMN_NAMES, get_k_best_for_one_method, df=phenotype_network, query_ids=query_id_values, mc.cores=numCores)
   table <- as.data.frame(matrix(unlist(table), nrow=length(unlist(table[1]))))
   names(table) <- PRED_COLUMN_NAMES
   write.csv(table, file=paste(OUTPUT_DIR,"phenotype_text_",gene_name,".csv",sep=""), row.names=FALSE)
@@ -90,12 +118,12 @@ for(query_id in query_ids_list){
 
 # Read in the phenotype network files output from the pipeline using the phene descriptions.
 phenotype_network <- read(NETWORKS_DIR,PHENE_TEXT_EDGES_FILE)
-for(query_id in query_ids_list){
+for(query in query_list){
   # Unpack the query ID information.
-  gene_name <- query_id[1]
-  query_id_values <- as.numeric(query_id[2:length(query_id)])
+  gene_name <- query[1]
+  query_id_values <- as.numeric(query[2:length(query)])
   # Generate the ranked list of similar genes.
-  table <- mclapply(PRED_COLUMN_NAMES, get_k_best_for_one_method, df=phenotype_network, query_id=query_id_values, mc.cores=numCores)
+  table <- mclapply(PRED_COLUMN_NAMES, get_k_best_for_one_method, df=phenotype_network, query_ids=query_id_values, mc.cores=numCores)
   table <- as.data.frame(matrix(unlist(table), nrow=length(unlist(table[1]))))
   names(table) <- PRED_COLUMN_NAMES
   write.csv(table, file=paste(OUTPUT_DIR,"phene_text_",gene_name,".csv",sep=""), row.names=FALSE)
@@ -169,7 +197,7 @@ ggplot(data=df_long, aes(x=bin,y=avg) ) + geom_bar(stat="identity") + geom_error
   facet_grid(dtype~method_factor, labeller=method_labeller) +
   theme_bw() +
   scale_x_discrete(breaks=c("bin_10","bin_100","bin_inf"), labels=c("1-10","11-100","100+")) +
-  scale_y_continuous(breaks=seq(0,8,1)) +
+  scale_y_continuous(breaks=seq(0,14,1)) +
   theme(plot.title = element_text(lineheight=1.0, face="bold", hjust=0.5), 
         legend.direction = "vertical", 
         legend.position = "right", 
@@ -179,7 +207,7 @@ ggplot(data=df_long, aes(x=bin,y=avg) ) + geom_bar(stat="identity") + geom_error
   xlab("Rank Value") + ylab("Rank Quantity")
 
 # Save the image of the plot.
-filename <- "/Users/irbraun/Desktop/pathway_example.png"
+filename <- "/Users/irbraun/Desktop/figure6_pathway_example.png"
 ggsave(filename, plot=last_plot(), device="png", path=NULL, scale=1, width=24, height=12, units=c("cm"), dpi=300, limitsize=TRUE)
 
 
