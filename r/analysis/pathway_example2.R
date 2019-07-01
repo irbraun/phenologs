d243c6172e391b0282dc4b0f5e42e95c0824422d
irbraun <irbraun@iastate.edu>
2019-07-01 09:44:21 -0500

    updating method for generating gene rankings

--- r/analysis/pathway_example2.R
+++ r/analysis/pathway_example2.R
@@ -44,23 +44,17 @@ read <- function(dir,filename){
 
 # IDs for the phenotypes and corresponding genes in the pathway.
 query_ids_list <- list(
-  c("GRMZM2G422750", 1262),
-  c("GRMZM2G005066", 1262),
-  c("GRMZM5G822829", 1262),
-  c("GRMZM2G172795", 1262),
-  c("GRMZM2G422750", 1262),
-  c("GRMZM2G026930", 1262),
-  c("GRMZM2G345717", 1262),
-  c("GRMZM2G165390", 1262),
-  c("GRMZM2G016241", 1262),
-  c("GRMZM2G005066", 1262),
-  c("GRMZM2G084799", 1262),
-  c("GRMZM2G701063", 1262),
-  c("GRMZM5G822829", 1262),
-  c("GRMZM2G172795", 1262)
+  c("GRMZM2G422750", 1262),                                   # c2
+  c("GRMZM2G026930", 1582,1583,1585,1979,2466,2709,171,213),  # a1
+  c("GRMZM2G345717", 174,214),                                # a2
+  c("GRMZM2G165390", 228,2329),                               # bz1
+  c("GRMZM2G016241", 229),                                    # bz2
+  c("GRMZM2G005066", 1261,170),                               # c1
+  c("GRMZM2G084799", 2467),                                   # p1
+  c("GRMZM2G701063", 169,1336),                               # pl1
+  c("GRMZM5G822829", 1728,2599,175,215,220,1263),             # r1
+  c("GRMZM2G172795", 173,1335,1584)                           # b1
 )
-
-
 
 
 
@@ -111,49 +105,82 @@ for(query_id in query_ids_list){
 
 
 
+
+
+
+
 # Making the figure.
 df <- read("/Users/irbraun/Desktop/", "big_table.csv")
 df$gene <- as.character(df$gene)
 
-# Average the values (bin quantities) for each method and text description type.
+# Average the values (bin quantities) for each method and text description type and get standard deviation.
 for (method in PRED_COLUMN_NAMES){
-  df[nrow(df)+1,] <- c("average", "Phenotype Descriptions", method, mean(df[(df$dtype=="Phenotype Descriptions") & (df$method==method),]$bin_10), 8.88, 8.88)
+  
+  sub_df <- df[(df$dtype=="Phenotype Descriptions") & (df$method==method),]
+  df[nrow(df)+1,] <- c("average", "Phenotype Descriptions", method,  mean(sub_df$bin_10), mean(sub_df$bin_100), mean(sub_df$bin_inf))
+  df[nrow(df)+1,] <- c("stdev", "Phenotype Descriptions", method, sd(sub_df$bin_10), sd(sub_df$bin_100), sd(sub_df$bin_inf))
   df$bin_10 <- as.numeric(df$bin_10)
   df$bin_100 <- as.numeric(df$bin_100)
   df$bin_inf <- as.numeric(df$bin_inf)
-  df[nrow(df)+1,] <- c("average", "Phene Descriptions", method, mean(df[(df$dtype=="Phene Descriptions") & (df$method==method),]$bin_10), 8.88, 8.88)
+  
+  sub_df <- df[(df$dtype=="Phene Descriptions") & (df$method==method),]
+  df[nrow(df)+1,] <- c("average", "Phene Descriptions", method,  mean(sub_df$bin_10), mean(sub_df$bin_100), mean(sub_df$bin_inf))
+  df[nrow(df)+1,] <- c("stdev", "Phene Descriptions", method, sd(sub_df$bin_10), sd(sub_df$bin_100), sd(sub_df$bin_inf))
   df$bin_10 <- as.numeric(df$bin_10)
   df$bin_100 <- as.numeric(df$bin_100)
   df$bin_inf <- as.numeric(df$bin_inf)
 }
-df <- df[df$gene=="average",]
 
-# Gathering the table data into the long format.
-df_long <- gather(df, bin, quantity, bin_10:bin_inf, factor_key=TRUE)
+# Averages.
+df_avg <- df[df$gene=="average",]
+df_long_avg <- gather(df_avg, bin, quantity, bin_10:bin_inf, factor_key=TRUE)
+names(df_long_avg)[5] <- "avg"
 
+# Standard deviations.
+df_sd <- df[df$gene=="stdev",]
+df_long_sd <- gather(df_sd, bin, quantity, bin_10:bin_inf, factor_key=TRUE)
+names(df_long_sd)[5] <- "sd"
 
-
-
-
-ggplot(data=df_long, aes(x=bin,y=quantity) ) + geom_bar(stat="identity") +
-  facet_wrap(~method)
-
-
-
-
-
-library(ggplot2)
-ggplot(data = data, mapping = aes(x = as.factor(zipcode), y = avgLSAmount)) +
-  geom_bar(stat = "identity") +
-  labs(x = "zipcode")
+# Combined.
+df_long <- merge(df_long_avg, df_long_sd, by=c("dtype","method","bin"))
 
 
 
 
 
+# Convert how methods are specified in the table to names that work in the figure and order as well.
+df_long$method_factor <- factor(df_long$method, levels=c("predefined", "pre_m1_edge", "pre_m2_edge", "jaccard", "cosine", "enwiki_dbow"))
+method_names <- list(
+  "predefined"="Curated EQs",
+  "pre_m1_edge"="Pred EQs S1",
+  "pre_m2_edge"="Pred EQs S2",
+  "enwiki_dbow"="Doc2Vec",
+  "jaccard"="Set-of-words",
+  "cosine"="Bag-of-words",
+  "Phenotype Descriptions"="Phenotype Descriptions",
+  "Phene Descriptions"="Phene Descriptions"
+)
+method_labeller <- function(value){return(method_names[value])}
+method_labeller <- as_labeller(method_labeller)
 
 
+# Make the plot.
+ggplot(data=df_long, aes(x=bin,y=avg) ) + geom_bar(stat="identity") + geom_errorbar(aes(ymin=avg-sd, ymax=avg+sd), width=.2) +
+  facet_grid(dtype~method_factor, labeller=method_labeller) +
+  theme_bw() +
+  scale_x_discrete(breaks=c("bin_10","bin_100","bin_inf"), labels=c("1-10","11-100","100+")) +
+  scale_y_continuous(breaks=seq(0,8,1)) +
+  theme(plot.title = element_text(lineheight=1.0, face="bold", hjust=0.5), 
+        legend.direction = "vertical", 
+        legend.position = "right", 
+        panel.grid.minor = element_line(color="lightgray"), 
+        panel.grid.major=element_blank(), 
+        axis.line=element_blank()) +
+  xlab("Rank Value") + ylab("Rank Quantity")
 
+# Save the image of the plot.
+filename <- "/Users/irbraun/Desktop/pathway_example.png"
+ggsave(filename, plot=last_plot(), device="png", path=NULL, scale=1, width=24, height=12, units=c("cm"), dpi=300, limitsize=TRUE)
 
 
 
